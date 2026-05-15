06fd6a470987ea9ed08ebbace373bf090d02d0ce
fatimamassbali-web <fatimamassbali@gmail.com>
2026-05-15 17:56:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 // Scroll to projects section
-const btn = document.querySelector(".btn");
+const btn = document.querySelector("#btnProjects");
 const projectsSection = document.querySelector("#projects");
 
 btn.addEventListener("click", () => {
   projectsSection.scrollIntoView({ behavior: "smooth" });
 });
 
+
 // Contact form interaction
-const form = document.querySelector("form");
+const form = document.querySelector("#contactForm");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -15,4 +16,23 @@ form.addEventListener("submit", (e) => {
   alert("Message sent successfully! Thank you 😊");
 
   form.reset();
+});
+ const openButtons = document.querySelectorAll(".open");
+
+openButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Opening project...");
+  });
+});
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const targetId = link.getAttribute("href");
+    const target = document.querySelector(targetId);
+
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
 });
